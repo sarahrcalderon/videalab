dc8007390406871b27752b6ffd96d16192c82a9d
sarahrcalderon <srcalderonn@gmail.com>
2025-04-05 13:49:14 -0300

    up

--- js/script.js
+++ js/script.js
@@ -1,11 +1,13 @@
 import { setupMenu } from './menu';
 import { aparecerImagem } from './efeitoImagem';
 import { scrollBar } from './scrollBar';
+import { efeitoVideo } from './efeitoVideo';
 
 document.addEventListener('DOMContentLoaded', () => {
   setupMenu();
   aparecerImagem();
   scrollBar();
+  efeitoVideo();
 
   // Outras inicializações podem vir aqui
 });
