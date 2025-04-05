1a5659e15d171973166700f3e3e948bf4a7d7537
sarahrcalderon <srcalderonn@gmail.com>
2025-04-05 15:59:32 -0300

    up

--- js/script.js
+++ js/script.js
@@ -3,6 +3,18 @@ import { aparecerImagem } from './efeitoImagem';
 import { scrollBar } from './scrollBar';
 import { efeitoVideo } from './efeitoVideo';
 
+document
+  .querySelectorAll('.formularioContato input, .formularioContato textarea')
+  .forEach((el) => {
+    el.addEventListener('focus', (e) => {
+      e.target.scrollIntoView({
+        behavior: 'auto',
+        block: 'nearest',
+        inline: 'nearest',
+      });
+    });
+  });
+
 document.addEventListener('DOMContentLoaded', () => {
   setupMenu();
   aparecerImagem();
