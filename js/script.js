92a176817b52d6e13f24da8de30a7bf38588bf41
sarahrcalderon <srcalderonn@gmail.com>
2025-04-07 13:43:12 -0300

    addgaleria

--- js/script.js
+++ js/script.js
@@ -2,11 +2,12 @@ import { setupMenu } from './menu';
 import { aparecerImagem } from './efeitoImagem';
 import { scrollBar } from './scrollBar';
 import { formulario } from './formulario';
-
+import { fraseEfeito } from './fraseEfeito ';
 document.addEventListener('DOMContentLoaded', () => {
   setupMenu();
   aparecerImagem();
   scrollBar();
+  fraseEfeito();
   formulario();
 
   // Outras inicializações podem vir aqui
