22a25557f3da32c7f68eb47a82ae695a42430c4c
sarahrcalderon <srcalderonn@gmail.com>
2025-07-16 12:30:38 -0300

    up

--- js/menu.js
+++ js/menu.js
@@ -2,18 +2,20 @@ document.addEventListener('DOMContentLoaded', function () {
   const menu = document.getElementById('menu');
   const botaoMenu = document.getElementById('menuBtn');
   const botaoFechar = document.getElementById('closeBtn');
-  const linksNavegacao = document.querySelectorAll('#menu a[href^="#"]'); // Seleciona todos os links âncora no menu
+  const linksNavegacao = document.querySelectorAll('#menu a[href^="#"]');
 
-  const abrirMenu = function (e) {
+  function abrirMenu(e) {
     e.stopPropagation();
     menu.classList.add('open');
+    botaoFechar.style.display = 'block';
     document.body.classList.add('menu-aberto');
-  };
+  }
 
-  const fecharMenu = function () {
+  function fecharMenu() {
     menu.classList.remove('open');
+    botaoFechar.style.display = 'none';
     document.body.classList.remove('menu-aberto');
-  };
+  }
 
   botaoMenu.addEventListener('click', abrirMenu);
 
@@ -23,10 +25,11 @@ document.addEventListener('DOMContentLoaded', function () {
   });
 
   document.addEventListener('click', function (e) {
-    const cliqueDentroDoMenu = menu.contains(e.target);
-    const cliqueNoBotaoMenu = botaoMenu.contains(e.target);
-
-    if (!cliqueDentroDoMenu && !cliqueNoBotaoMenu) {
+    if (
+      !menu.contains(e.target) &&
+      !botaoMenu.contains(e.target) &&
+      menu.classList.contains('open')
+    ) {
       fecharMenu();
     }
   });
@@ -41,7 +44,6 @@ document.addEventListener('DOMContentLoaded', function () {
       const idAlvo = this.getAttribute('href');
       fecharMenu();
 
-      // Rola suavemente para a seção após um pequeno delay
       setTimeout(() => {
         const secaoAlvo = document.querySelector(idAlvo);
         if (secaoAlvo) {
